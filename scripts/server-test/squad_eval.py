7301aa6f1860770c7ba5e1487d33c1d8dcd50b56
UMXihao <sunxh2016@gmail.com>
2026-05-23 21:38:26 +0800

    opencl analysis

--- scripts/server-test/squad_eval.py
+++ scripts/server-test/squad_eval.py
@@ -12,6 +12,17 @@ from datasets import load_dataset
 import evaluate
 import numpy as np
 
+def trimmed_mean_np(arr):
+    arr = np.asarray(arr)
+
+    if arr.size <= 6:
+        raise ValueError("数组长度必须大于 6")
+
+    sorted_arr = np.sort(arr)
+    trimmed = sorted_arr[3:-3]
+
+    return np.mean(trimmed)
+
 def percentile(data, percentile):
     return float(np.percentile(data, percentile, method="linear"))
 
@@ -29,10 +40,11 @@ squad_val = load_dataset("squad", split="validation")
 predictions = []
 references = []
 prefill = []
+token_num = []
 decode = []
 
 start_time = time.time()
-for i in tqdm(range(100)):
+for i in tqdm(range(10)):
 #for i in tqdm(range(len(squad_val))):
     context = squad_val[i]["context"]
     question = squad_val[i]["question"]
@@ -52,9 +64,11 @@ for i in tqdm(range(100)):
     reference = {'answers': answers, 'id': question_id}
     prediction = {'prediction_text': predict, 'id': question_id}
     prompt_ms = response.json().get("timings").get("prompt_ms")
+    prompt_per_second = response.json().get("timings").get("prompt_per_second")
     predicted_per_token_ms = response.json().get("timings").get("predicted_per_token_ms")
     # print("TTFT: ", prompt_ms, " TPOT: ", predicted_per_token_ms)
     prefill.append(prompt_ms)
+    token_num.append(prompt_per_second)
     decode.append(predicted_per_token_ms)
 
     references.append(reference)
@@ -68,14 +82,34 @@ elapsed_time = end_time - start_time
 
 print(f"requests: {elapsed_time:.2f} seconds")
 print("==========PREFILL============")
+print("MEAN = ", trimmed_mean_np(prefill), " ms")
 print("P50 = ", percentile(prefill, 50), " ms")
 print("P90 = ", percentile(prefill, 90), " ms")
 print("P99 = ", percentile(prefill, 99), " ms")
+print("==========PREFILL TOKEN NUMBER============")
+print("MEAN = ", trimmed_mean_np(token_num))
+print("P50 = ", percentile(token_num, 50))
+print("P90 = ", percentile(token_num, 90))
+print("P99 = ", percentile(token_num, 99))
 print("==========DECODE============")
+print("MEAN = ", trimmed_mean_np(decode), " ms")
 print("P50 = ", percentile(decode, 50), " ms")
 print("P90 = ", percentile(decode, 90), " ms")
 print("P99 = ", percentile(decode, 99), " ms")
-
+print(
+    f"{trimmed_mean_np(prefill):.2f},"
+    f"{np.percentile(prefill, 50):.2f},"
+    f"{np.percentile(prefill, 90):.2f},"
+    f"{np.percentile(prefill, 99):.2f},"
+    f"{trimmed_mean_np(token_num):.2f},"
+    f"{np.percentile(token_num, 50):.2f},"
+    f"{np.percentile(token_num, 90):.2f},"
+    f"{np.percentile(token_num, 99):.2f},"
+    f"{trimmed_mean_np(decode):.2f},"
+    f"{np.percentile(decode, 50):.2f},"
+    f"{np.percentile(decode, 90):.2f},"
+    f"{np.percentile(decode, 99):.2f}"
+)
 '''
 ./build/bin/llama-server -m /data/DeepSeek-Coder-V2-Lite-Instruct-IQ2_XS.gguf -ngl 33
 
